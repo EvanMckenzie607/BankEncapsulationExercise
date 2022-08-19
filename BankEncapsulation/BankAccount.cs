0248c5456eb86ebcb78f1fd8f88cb65922919b5a
Evan Mckenzie <615evan@gmail.com>
2022-08-19 13:07:48 -0400

    Completed exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public  void Deposit(double money)
+        {
+            _balance += money;
+             
+        }
+        public void Withdraw(double money)
+        {
+            _balance -= money;
+        }
+        public double GetBalance()
+        {
+            Console.WriteLine("Your balance is now:");
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using BankEncapsulation;
+//***EncapsulationExercise***//
+
+
+BankAccount userAccount = new BankAccount();
+
+Console.WriteLine("hello! Thanks for choosing Bank of Lost Dreams");
+Console.WriteLine("How much do you want to deposit?");
+
+var amountToDeposit = double.Parse(Console.ReadLine());
+userAccount.Deposit(amountToDeposit);
+
+Console.WriteLine(userAccount.GetBalance());
+Console.WriteLine("How much would you like to withdraw?");
+
+var amountToWithdraw = double.Parse(Console.ReadLine());
+userAccount.Withdraw(amountToWithdraw);
+
+Console.WriteLine(userAccount.GetBalance());
+
+
+
+
+
+
+
+
+
+
+
